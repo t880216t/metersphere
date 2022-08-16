2b6c903b7def1c6283d3c991f8025dee046ea7e7
chenjian0112 <chenjian0112@made-in-china.com>
2022-08-16 20:54:43 +0800

    脚本更新

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ mkdir -p backend/target/dependency && (cd backend/target/dependency; jar -xf ../
 docker rmi mic_metersphere:2.0.1
 rm mic_metersphere_2.0.1.tar
 docker build --build-arg MS_VERSION=2.0.1 -t mic_metersphere:2.0.1 .
-docker save -o mic_metersphere_2.0.1.tar mic_metersphere:2.0.1
+docker save -o mic_metersphere.tar mic_metersphere:2.0.1
 python3 -m http.server
